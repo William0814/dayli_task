26279357759a11a7a57db558e2b4db9389d8a928
william0814 <king-master90@hotmail.com>
2025-01-14 23:08:59 +0100

    little change

--- big_secund_number.py
+++ big_secund_number.py
@@ -1,5 +1,5 @@
 """Tu tarea es escribir un programa que tome una tupla de números, encuentre el segundo número más grande 
-e imprima ese número, lña tupla es --numbers = (10, 5, 8, 20, 15)--"""
+e imprima ese número, la tupla es --numbers = (10, 5, 8, 20, 15)--"""
 numbers = (10, 5, 8, 20, 15)
 list1 = list(numbers)
 list1.sort()
