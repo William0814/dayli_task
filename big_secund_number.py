ae7f93d85d5009739744899d7c5cf070dcd7daf1
william0814 <king-master90@hotmail.com>
2025-01-14 19:47:58 +0100

    Of a tuple have the secund number more big

---
+++ big_secund_number.py
@@ -0,0 +1,12 @@
+"""Tu tarea es escribir un programa que tome una tupla de números, encuentre el segundo número más grande 
+e imprima ese número, lña tupla es --numbers = (10, 5, 8, 20, 15)--"""
+numbers = (10, 5, 8, 20, 15)
+list1 = list(numbers)
+list1.sort()
+print(f'The secund number more big is: {list1[3]}')
+
+
+"""for i in list1:
+maximo = max(numbers)
+print(maximo)"""
+   
